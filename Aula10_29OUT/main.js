76e859143b2957f13d36deee5ffd3c64b8d324d7
Marcelo <marc.m.amorim@gmail.com>
2025-10-29 22:33:38 -0300

    v1

--- Aula10_29OUT/main.js
+++ Aula10_29OUT/main.js
@@ -29,3 +29,30 @@ let cifrado = cifrarAtbash("ALFACE cenoura")
 console.log("cifrado", cifrado);
 let decifrado = cifrarAtbash(cifrado)
 console.log("decifrado", decifrado);
+
+function cifrarAtbash2(texto) {
+    let saida = ""
+    for (let i = 0; i < texto.length; i++) {
+        let letterIn = texto[i]
+        let letterOut = " "
+        if (letterIn != " ") {
+            if(letterIn.charCodeAt(0) <= 90) {
+                let codigo = 90 - (letterIn.charCodeAt(0)-65) 
+                letterOut = String.fromCharCode(codigo)
+            } else {
+                let codigo = 122 - (letterIn.charCodeAt(0)-97) 
+                letterOut = String.fromCharCode(codigo)
+            }
+            //console.log("codigo",codigo);
+        }
+        //console.log("letterOut",letterOut);
+        saida = saida + letterOut
+    }
+    //console.log("saida",saida);
+    return saida
+}
+
+let cifrado2 = cifrarAtbash2("ALFACE alface")
+console.log("cifrado2", cifrado2);
+let decifrado2 = cifrarAtbash2(cifrado2)
+console.log("decifrado", decifrado2);
